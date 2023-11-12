27384b34f00d67a9e03af0bbc84143a5f2612553
Jonathan Rwabahizi <jonathanrwabahizi@gmail.com>
2023-11-12 22:47:42 +0200

    first commit

---
+++ src/main/java/com/example/iso8583/Iso8583Application.java
@@ -0,0 +1,13 @@
+package com.example.iso8583;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iso8583Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iso8583Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iso8583/Iso8583ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iso8583;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iso8583ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
